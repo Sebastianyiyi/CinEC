e3436e03c3fbc632e7b6a64f712bbea95f7b0646
Erick <loericklo98@gmail.com>
2026-04-14 12:06:23 -0500

    feat: incluye la pagina de promociones dentro de las rutas habilitadas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Promociones from "./pages/Promociones";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/promociones" element={<Promociones />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
