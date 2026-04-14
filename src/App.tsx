be03df98932cdf0f9725ba77adadeb482a5c417b
Vladimir Gonzalez <umberlarez@gmail.com>
2026-04-13 22:06:09 -0500

    feat: registra ruta principal en App.tsx apuntando a página Index

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-      {/*Aquí deben poner las rutas*/}
+        <Route path="/" element={<Index />} />
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   </QueryClientProvider>
-)
+);
 
 export default App;
