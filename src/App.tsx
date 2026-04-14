c50684dadbd3a77b0b875ef9b3d2ed5824ffd490
Erick <loericklo98@gmail.com>
2026-04-14 12:07:02 -0500

    feat: define la ruta dinamica para ver los detalles de una pelicula

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import MovieDetail from "./pages/MovieDetail";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/pelicula/:id" element={<MovieDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
