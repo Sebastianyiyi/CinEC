a273281ab6ca4711c523a66317eb8377ea5ceec5
Sebastián <sebastianacaro6@gmail.com>
2026-04-15 00:43:23 -0500

    feat: Elimina imports repetidos

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index";
-import Cartelera from "./pages/Cartelera";
-import MovieDetail from "./pages/MovieDetail";
 import Booking from "./pages/Booking";
 import Snacks from "./pages/Snacks";
 import Estrenos from "./pages/Estrenos";
